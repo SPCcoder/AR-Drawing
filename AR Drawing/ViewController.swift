3785f85311479c580effbca7386a00f731d89ed6
Sean Carlin <spcarlin@hotmail.com>
2017-12-11 23:59:42 +0000

    comments

--- AR Drawing/ViewController.swift
+++ AR Drawing/ViewController.swift
@@ -17,9 +17,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
-        self.sceneView.session.run(configuration)
-        self.sceneView.showsStatistics = true
+      //  self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+      //  self.sceneView.session.run(configuration)
+      //  self.sceneView.showsStatistics = true
         self.sceneView.delegate = self
     }
     @IBAction func drawButtonPressed(_ sender: Any) {
@@ -28,7 +28,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
         // 60fps
-        guard let pointOfView = sceneView.pointOfView else {return}
+        guard let pointOfView = sceneView.pointOfView else {return} // stops crash if we have no point of view
         
         let transform = pointOfView.transform
         let orientation = SCNVector3(-transform.m31,-transform.m32,-transform.m33)// transform matrix - need to learn more about this, 3rd col, row 1
@@ -36,16 +36,16 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let currentPostionOfCamera = orientation + location
         //print(orientation.x, orientation.y,orientation.z)
         
-        DispatchQueue.main.async {
-            if self.drawButton.isHighlighted {
+        DispatchQueue.main.async {// we draw on main thread to stop crash
+            if self.drawButton.isHighlighted {// this is the drawing we keep
                 let sphereNode = SCNNode.init(geometry: SCNSphere.init(radius: 0.02))
                 sphereNode.position = currentPostionOfCamera
                 self.sceneView.scene.rootNode.addChildNode(sphereNode)
                 sphereNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
                 
-            }else {
+            }else {// so user can see their drawing points
                 let pointer = SCNNode.init(geometry: SCNSphere.init(radius: 0.01))
-                pointer.name = "pointer"
+                pointer.name = "pointer" //we delete nodes with name pointer
                 pointer.position = currentPostionOfCamera
                 
                 self.sceneView.scene.rootNode.enumerateChildNodes({ (node, _) in
