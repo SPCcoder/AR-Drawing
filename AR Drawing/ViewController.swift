5c55c52f1479b5a547392583e9968b8060bdc483
Sean Carlin <spcarlin@hotmail.com>
2017-12-11 23:41:46 +0000

    first commit, UI, getting camera position, allow camera in plist

--- AR Drawing/ViewController.swift
+++ AR Drawing/ViewController.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
+import ARKit
+class ViewController: UIViewController, ARSCNViewDelegate {
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+        self.sceneView.showsStatistics = true
+        self.sceneView.delegate = self
+    }
+    @IBAction func drawButtonPressed(_ sender: Any) {
+        
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
+        // 60fps
+        guard let pointOfView = sceneView.pointOfView else {return}
+        
+        let transform = pointOfView.transform
+        let orientation = SCNVector3(-transform.m31,-transform.m32,-transform.m33)// transform matrix - need to learn more about this, 3rd col, row 1
+        let location = SCNVector3(transform.m41,transform.m42,transform.m43)
+        let currentPostionOfCamera = orientation + location
+        print(orientation.x, orientation.y,orientation.z)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
 
+func +(left: SCNVector3, right: SCNVector3)->SCNVector3{
+    return SCNVector3Make(left.x + right.x, left.y + right.y, left.z + right.z)
+}
